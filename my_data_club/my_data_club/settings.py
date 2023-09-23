bad2ef8549fd8189ac7594975044c9f1c92f886c
hectorManu <le19081442@itmerida.edu.mx>
2023-09-22 20:43:55 -0600

    add notes

--- my_data_club/my_data_club/settings.py
+++ my_data_club/my_data_club/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@2cg#=l-*72artfjb_^!%&2*%x&n@021gxspxp^vb4bsvurdh_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
